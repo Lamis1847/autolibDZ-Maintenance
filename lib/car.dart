9a5903ef182326ea6f29464ca7c23c81eb4ee8cb
LAGHOUB Nassim <hn_laghoub@esi.dz>
2021-05-12 00:28:06 +0100

    carPage

--- lib/car.dart
+++ lib/car.dart
@@ -20,28 +20,31 @@ class _CarState extends State<Car> {
       Container(
         height: double.infinity,
         width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            stops: [2/3 , 1/3],
-            colors: [
-              Color(0xff252834),
-              Color(0xffFFCB00),
-            ]
-           ),
-        ),
+        color: const Color(0xff252834),
         child: SingleChildScrollView(
                   child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 children: <Widget>[
                   Row(children: [
+                    Flat(
+                                          child: Icon(
+                            Icons.arrow_back_sharp,
+                            color: Colors.white,
+                            
+                          ),
+                    ),
+                        SizedBox(
+                          width:10,
+                        ),
+
                     Expanded(
                       flex: 3,
                      child: Container(
-                        child: Text("Bugatti Chiron",
+                        child: Text("Peugeot 308",
                         style:TextStyle(
                           fontFamily: 'Nunito',
-                          fontSize: long*0.035,
+                          fontSize: long*0.045,
                           fontWeight: FontWeight.w600,
                           color: const Color(0xffffffff),
                         )),
@@ -52,176 +55,17 @@ class _CarState extends State<Car> {
                       child: Container(
                       )),
                   ],),
+                   Divider(
+                    color: Colors.white,
+              
+                  ),
+                  SizedBox(
+                  height:30,
+                ),
 
-                  
-
-                  Row(
-                    children:<Widget>[
-                        Column(
-                          children:<Widget>[
-          Container(
-            height: long*0.12,
-            width: larg*0.35,
-            child: Card(  
-            child: 
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-               children:<Widget>[
-                 Text("Carburant",
-                 style:TextStyle(
-                   fontFamily: 'Nunito',
-                   fontSize: 16,
-                   fontWeight: FontWeight.w400,
-                   color: Colors.black,
-                 ),
-                 ),
-                 SizedBox(
-                   height:3,
-                 ),
-
-                 Text("100 km",
-                 textAlign: TextAlign.left,
-                 style:TextStyle(
-                   fontFamily: 'Nunito',
-                   fontSize: 14,
-                   fontWeight: FontWeight.w400,
-                   color: const Color(0xff667C8A),
-                 ),
-                 ),
-               ] 
-              ),
-            ),
-            ),
-          ),
-
-          SizedBox(
-            height:5,
-          ),
-          Container(
-            height: long*0.12,
-            width: larg*0.35,
-            child: Card(  
-            child: 
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-               children:<Widget>[
-                 Text("Kilométrage",
-                 style:TextStyle(
-                   fontFamily: 'Nunito',
-                   fontSize: 16,
-                   fontWeight: FontWeight.w400,
-                   color: Colors.black,
-                 ),
-                 ),
-                 SizedBox(
-                   height:3,
-                 ),
-
-                 Text("45 254 km",
-                 textAlign: TextAlign.left,
-                 style:TextStyle(
-                   fontFamily: 'Nunito',
-                   fontSize: 14,
-                   fontWeight: FontWeight.w400,
-                   color: const Color(0xff667C8A),
-                 ),
-                 ),
-               ] 
-              ),
-            ),
-            ),
-          ),
-
-          SizedBox(
-            height:5,
-          ),
-          Container(
-           height: long*0.12,
-            width: larg*0.35,
-            child: Card(  
-            child: 
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-               children:<Widget>[
-                 Text("Matricule",
-                 style:TextStyle(
-                   fontFamily: 'Nunito',
-                   fontSize: 16,
-                   fontWeight: FontWeight.w400,
-                   color: Colors.black,
-                 ),
-                 ),
-                 SizedBox(
-                   height:3,
-                 ),
-
-                 Text("0023 120 16",
-                 textAlign: TextAlign.left,
-                 style:TextStyle(
-                   fontFamily: 'Nunito',
-                   fontSize: 14,
-                   fontWeight: FontWeight.w400,
-                   color: const Color(0xff667C8A),
-                 ),
-                 ),
-               ] 
-              ),
-            ),
-            ),
-          ),
-
-          SizedBox(
-            height:5,
-          ),
-          Container(
-            height: long*0.12,
-            width: larg*0.35,
-            child: Card(  
-            child: 
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-               children:<Widget>[
-                 Text("Etat",
-                 style:TextStyle(
-                   fontFamily: 'Nunito',
-                   fontSize: 16,
-                   fontWeight: FontWeight.w400,
-                   color: Colors.black,
-                 ),
-                 ),
-                 SizedBox(
-                   height:3,
-                 ),
-
-                 Text("En service",
-                 textAlign: TextAlign.left,
-                 style:TextStyle(
-                   fontFamily: 'Nunito',
-                   fontSize: 14,
-                   fontWeight: FontWeight.w400,
-                   color: const Color(0xff667C8A),
-                 ),
-                 ),
-               ] 
-              ),
-            ),
-            ),
-          ),
-                          ]
-                        ),
-                        Column(
-                          children: [
-          Container(
-          width: 181,
-          height: 353,
+                  Container(
+                  width: 282,
+                  height: 96,
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: const AssetImage('img/car.png'),
@@ -229,30 +73,204 @@ class _CarState extends State<Car> {
                     ),
                   ),
                 ),
-          FlutterSwitch(
-                          activeText: "En service",
-                          inactiveText: "Hors service",
-                          activeColor: const Color(0xff2DCE89),
-                          inactiveColor: const Color(0xffF5365C),
-                          activeTextFontWeight: FontWeight.bold,
-                          activeTextColor: Colors.white,
-                          inactiveTextColor: Colors.white,
-                          value: status,
-                          valueFontSize: 12.0,
-                          width: 130,
-                          borderRadius: 30.0,
-                          showOnOff: true,
-                          onToggle: (val) {
-          setState(() {
-            status = val;
-          });
-                          },
+                SizedBox(
+                  height:30,
+                ),
+
+                  Row(
+                       mainAxisAlignment: MainAxisAlignment.center,
+
+                    children:<Widget>[
+                        Container(
+                          height: long*0.12,
+                          width: larg*0.35,
+                          child: Card(  
+                          child: 
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                             children:<Widget>[
+                               Text("Carburant",
+                               style:TextStyle(
+                                 fontFamily: 'Nunito',
+                                 fontSize: 16,
+                                 fontWeight: FontWeight.w400,
+                                 color: Colors.black,
+                               ),
+                               ),
+                               SizedBox(
+                                 height:3,
+                               ),
+
+                               Text("100 km",
+                               textAlign: TextAlign.left,
+                               style:TextStyle(
+                                 fontFamily: 'Nunito',
+                                 fontSize: 14,
+                                 fontWeight: FontWeight.w400,
+                                 color: const Color(0xff667C8A),
+                               ),
+                               ),
+                             ] 
+                            ),
+                          ),
+                          ),
                         ),
-                          
+
+                        SizedBox(
+                          width:5,
+                        ),
+                        Container(
+                          height: long*0.12,
+                          width: larg*0.35,
+                          child: Card(  
+                          child: 
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                             children:<Widget>[
+                               Text("Kilométrage",
+                               style:TextStyle(
+                                 fontFamily: 'Nunito',
+                                 fontSize: 16,
+                                 fontWeight: FontWeight.w400,
+                                 color: Colors.black,
+                               ),
+                               ),
+                               SizedBox(
+                                 height:3,
+                               ),
+
+                               Text("45 254 km",
+                               textAlign: TextAlign.left,
+                               style:TextStyle(
+                                 fontFamily: 'Nunito',
+                                 fontSize: 14,
+                                 fontWeight: FontWeight.w400,
+                                 color: const Color(0xff667C8A),
+                               ),
+                               ),
+                             ] 
+                            ),
+                          ),
+                          ),
+                        ),
+
+                         ]
+                  ),
+                        SizedBox(
+                          height:5,
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+
+                          children: [
+                            Container(
+                             height: long*0.12,
+                              width: larg*0.35,
+                              child: Card(  
+                              child: 
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                 children:<Widget>[
+                                   Text("Matricule",
+                                   style:TextStyle(
+                                     fontFamily: 'Nunito',
+                                     fontSize: 16,
+                                     fontWeight: FontWeight.w400,
+                                     color: Colors.black,
+                                   ),
+                                   ),
+                                   SizedBox(
+                                     height:3,
+                                   ),
+
+                                   Text("0023 120 16",
+                                   textAlign: TextAlign.left,
+                                   style:TextStyle(
+                                     fontFamily: 'Nunito',
+                                     fontSize: 14,
+                                     fontWeight: FontWeight.w400,
+                                     color: const Color(0xff667C8A),
+                                   ),
+                                   ),
+                                 ] 
+                                ),
+                              ),
+                              ),
+                            ),
+                            SizedBox(
+                          width:5,
+                        ),
+                        Container(
+                          height: long*0.12,
+                          width: larg*0.35,
+                          child: Card(  
+                          child: 
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                             children:<Widget>[
+                               Text("Etat",
+                               style:TextStyle(
+                                 fontFamily: 'Nunito',
+                                 fontSize: 16,
+                                 fontWeight: FontWeight.w400,
+                                 color: Colors.black,
+                               ),
+                               ),
+                               SizedBox(
+                                 height:3,
+                               ),
+
+                               Text("En service",
+                               textAlign: TextAlign.left,
+                               style:TextStyle(
+                                 fontFamily: 'Nunito',
+                                 fontSize: 14,
+                                 fontWeight: FontWeight.w400,
+                                 color: const Color(0xff667C8A),
+                               ),
+                               ),
+                             ] 
+                            ),
+                          ),
+                          ),
+                        ),
                           ],
                         ),
-                    ]
-                  ),
+                        SizedBox(
+                          height:25,
+                        ),
+
+                        
+                        FlutterSwitch(
+                                        activeText: "En service",
+                                        inactiveText: "Hors service",
+                                        activeColor: const Color(0xff2DCE89),
+                                        inactiveColor: const Color(0xffF5365C),
+                                        activeTextFontWeight: FontWeight.bold,
+                                        activeTextColor: Colors.white,
+                                        inactiveTextColor: Colors.white,
+                                        value: status,
+                                        valueFontSize: 12.0,
+                                        width: 130,
+                                        borderRadius: 30.0,
+                                        showOnOff: true,
+                                        onToggle: (val) {
+                        setState(() {
+                          status = val;
+                        });
+                                        },
+                                      ),
+                   SizedBox(
+                     height:20,
+                   ),
 
                   Container(
                   padding: EdgeInsets.symmetric(vertical:5),
@@ -305,7 +323,7 @@ class _CarState extends State<Car> {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(4),
                     ),
-                    color: const Color(0xffF5F3F5), 
+                    color: const Color(0xffFFCB00), 
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
